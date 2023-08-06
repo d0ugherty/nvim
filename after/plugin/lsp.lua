383edfdb5294d6008a9e199871d0f701a42f0003
thomas <tdougherty16@gmail.com>
2023-08-05 21:20:13 -0400

    autocompletion keymapping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,7 +15,7 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<Cdd>'] = cmp.mapping.confirm({ select = true }),
+	['<C-d>'] = cmp.mapping.confirm({ select = true }),
 	["<C-space>"] = cmp.mapping.complete(),
 })
 
