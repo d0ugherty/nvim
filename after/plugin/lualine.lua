5fae24a90d4297b4dcb645f0fb07220ae635391b
d0ugherty <doughe38@students.rowan.edu>
2023-12-02 13:23:53 -0500

    adjusted sections and background of status bar

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -28,18 +28,25 @@ require('lualine').setup {
                     removed = {fg = '#ff5555'},
                     }
                 },
-                'diagnostics'
+                'diagnostics',
             },
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
+    lualine_x = {},
+    lualine_y = {'progress',
+                 {'encoding',
+                    color = {bg = theme}
+                }, 
+                {'fileformat',
+                    color = { bg = theme}
+                },
+                 'filetype'},
     lualine_z = {'location'}
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
