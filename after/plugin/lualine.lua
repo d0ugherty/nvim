d842a21c51e50818075cd0e19826d9d5e3062291
d0ugherty <doughe38@students.rowan.edu>
2023-12-02 12:36:08 -0500

    adjusted lualine diff colors

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -19,7 +19,17 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {
+               'branch', 
+               { 'diff',
+                diff_color = {
+                    added = {fg = '#7bce43'},
+                    modified = {fg = '#d8a236'},
+                    removed = {fg = '#ff5555'},
+                    }
+                },
+                'diagnostics'
+            },
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
